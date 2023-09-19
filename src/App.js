a8053c0a6db5b1fabeb53b5f4035f45276904855
박준형 <revil.com@gmail.com>
2023-09-19 23:17:42 +0900

    feat: fragment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>리액트</h1>
+      <h2>뉴비입니다</h2>
+    </>
   );
 }
 
